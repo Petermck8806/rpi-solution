6abe481bc6a15b0fa6ab1f2401b69017acf1844b
Pete <pmckinney8806@gmail.com>
2015-04-01 15:58:39 -0500

    Updated Flask folder structure. Created test app.

--- rpi-web/hello.py
+++ rpi-web/hello.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+  return render_template('hello.html', name=name)
+
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
+def index():
+  return 'Welcome'
 
 if __name__ == '__main__':
   app.run()
