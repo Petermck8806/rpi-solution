9574a6bd9599358d66697a4398799de9fc6ae2b7
Pete <pmckinney8806@gmail.com>
2015-04-02 14:30:33 -0500

    Updated 404. Changed name of master file.
    Changed the name of the hello.py file to web_main.py.

--- rpi-web/web_main.py
+++ rpi-web/web_main.py
@@ -13,14 +13,22 @@ def hello_world(name=None):
 def index():
   return render_template('index.html')
 
+
 @app.route('/dashboard')
 def dashboard():
   return render_template('dashboard.html')
 
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+
+#error handler methods
 @app.errorhandler(404)
 def page_not_found(error):
   return render_template('page_not_found.html'), 404
+
 
 if __name__ == '__main__':
   app.run()
